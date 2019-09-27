50da3129db5758f5041cbe73f1174898b99ca5f5
Grytsenko <MGrytsenko@kiev.luxoft.com>
2019-09-27 17:35:06 +0300

    Initial commit

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HomePage {
+
+    private By formAuthentication = By.linkText("Form Authentication");
+    public void clickFormAuthentication(){
+
+    }
+
+}
